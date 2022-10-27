e16d44ee9f88e1875a8fbba66cebb3779b69c31d
GoGoGao <61309197+GoGoGao@users.noreply.github.com>
2022-10-27 19:05:58 +0800

    Add files via upload

---
+++ fig5c.unweighted.heatmap/heatmap.r
@@ -0,0 +1,9 @@
+library(pheatmap)
+x<-read.table("total.vfdb.even.rpm.mat01.reorder.xls",sep="\t",header=T,row.names=1)
+group<-read.table("sort.group.list",sep="\t",header=F)
+annotation_row = data.frame(Group=factor(group$V2))
+rownames(annotation_row) = group$V1
+ann_colors=list(Group=c("NC"="#808080","Infected"="#DC143C","Uninfected"="#20B2AA"))
+pdf(file="heatmap.pdf",width=11,heigh=7)
+pheatmap(t(x),fontsize=15,scale="none",cluster_cols=T,color = colorRampPalette(c("white","black"))(100),show_colnames=F,treeheight_row=80,treeheight_col=80,cluster_rows=F,annotation_row = annotation_row,annotation_colors=ann_colors,show_rownames=F,legend=F)   
+dev.off()
